5e1cd668e2ab5b6f148489942dd89a967e6782c8
Emmanuel Donkor <edonkor0000@gmail.com>
2023-07-19 23:32:54 +0200

    Added Course Repository

---
+++ src/main/java/com/emmanueldonkor/spring/data/jpa/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.emmanueldonkor.spring.data.jpa.repository;
+
+import com.emmanueldonkor.spring.data.jpa.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
