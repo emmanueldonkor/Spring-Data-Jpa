16b6ea680f1c02b692ebfe684d718af2fa5452d8
Emmanuel Donkor <edonkor0000@gmail.com>
2023-07-19 23:33:36 +0200

    Added Student Repository

--- src/main/java/com/emmanueldonkor/spring/data/jpa/repository/StudentRepository.java
+++ src/main/java/com/emmanueldonkor/spring/data/jpa/repository/StudentRepository.java
@@ -1,10 +1,34 @@
 package com.emmanueldonkor.spring.data.jpa.repository;
 
 import com.emmanueldonkor.spring.data.jpa.entity.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository  extends JpaRepository<Student, Long> {
+ public List<Student> findByFirstName(String firstName);
 
+ //JPQL
+  @Query("select s.firstName from Student s where s.emailId = ?1")
+    String getStudentFirstNameByEmailAddress(String emailId);
+
+  //Native
+  @Query(
+    value = "SELECT * FROM tbl_student s where s.email_address = ?1",
+    nativeQuery = true
+  )
+  Student getStudentEmailAddressNative(String emailId);
+
+  @Modifying
+  @Transactional
+  @Query(
+    value ="updapate tbl_student set first_name =?1 where email_address = ?2",
+    nativeQuery = true
+  )
+  int updateStudentNameByEmailId(String firstName, String emailId);
 }
