53c9f95be0cb083c76c64234644e09d79959cdee
Emmanuel Donkor <edonkor0000@gmail.com>
2023-07-20 23:55:19 +0200

    Added Student Repository Test

--- src/test/java/com/emmanueldonkor/spring/data/jpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/emmanueldonkor/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.emmanueldonkor.spring.data.jpa.repository;
 
+import com.emmanueldonkor.spring.data.jpa.entity.Guardian;
 import com.emmanueldonkor.spring.data.jpa.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +21,39 @@ class StudentRepositoryTest {
       .emailId("emmanuelmk42@gmail.com")
       .firstName("Emmanuel")
       .lastName("Donkor")
-      .guardianName("David")
-      .guardianEmail("davidonkor42@gmail.com")
-      .guardianMobile("96947577374")
+     // .guardianName("David")
+     // .guardianEmail("davidonkor42@gmail.com")
+     // .guardianMobile("96947577374")
       .build();
     studentRepository.save(student);
+  }
+
+  @Test
+  public void saveStudentWithGuardian(){
+    Guardian guardian = Guardian.builder()
+      .email("daviddonkor@gmail.com")
+      .name("David")
+      .mobile("468686848")
+      .build();
+
+   Student student = Student.builder()
+     .firstName("Emmanuel")
+     .emailId("emmanuelmk4@gmail.com")
+     .lastName("Donkor")
+     .guardian(guardian)
+     .build();
+   studentRepository.save(student);
   }
   @Test
   public void printAllStudent(){
     List<Student> studentList = studentRepository.findAll();
     System.out.println("studentList =" + studentList);
   }
+ @Test
+  public void printStudentByFirstName(){
+  List<Student> studentList =   studentRepository.findByFirstName("Emmanuel");
+   System.out.println("StudentList=" + studentList);
+ }
 
 
 }
